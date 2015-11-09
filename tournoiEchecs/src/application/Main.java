ec06580d00388cbdc763db334d6f3e18c5de76ed
AntoineGrunwald <ant.grunwald@gmail.com>
2015-11-09 21:55:30 +0100

    test commit 2 ag

--- tournoiEchecs/src/application/Main.java
+++ tournoiEchecs/src/application/Main.java
@@ -11,6 +11,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			String str = "test";
+			String str2 = "test2";
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
