c822def57ee77253af762efacf9e9e561c6193e9
Nico <Nico@PC-Nico>
2015-11-12 11:01:50 +0100

    Création des tables Tournoi et Joueur Création fenetre tournoi+fenetre Joueurs

--- tournoiEchecs/src/vue/CreationTournoi.java
+++ tournoiEchecs/src/vue/CreationTournoi.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -15,7 +16,7 @@ public class CreationTournoi extends Stage{
 		super();
 		try {
 			FXMLLoader leLoader = new FXMLLoader(this.getClass().getResource("/vue/creationTournoi.fxml"));
-			BorderPane root = leLoader.load();
+			AnchorPane root = leLoader.load();
 			this.initModality(Modality.WINDOW_MODAL);
 			this.initOwner(window);
 			Scene scene = new Scene(root);
