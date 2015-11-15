9576b761bd8418a18d62987ec9966e69b442ba40
Nico <Nico@192.168.1.58>
2015-11-15 16:02:39 +0100

    gestion conflits

---
+++ tournoiEchecs/src/controleur/ControleurAjouterJoueurTournoi.java
@@ -0,0 +1,60 @@
+package controleur;
+
+import java.lang.reflect.Array;
+import java.net.URL;
+
+
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+import metier.Joueur;
+import javafx.beans.property.SimpleStringProperty;
+
+
+import metier.Joueur;
+import metier.TestJooueur;
+import modele.ModeleJoueur;
+import javafx.beans.value.ChangeListener;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class ControleurAjouterJoueurTournoi implements Initializable {
+	
+	private ObservableList<TestJooueur> data = FXCollections.observableArrayList();
+
+	@FXML
+	private ListView<TestJooueur> listePersonne;
+	
+	@FXML
+	TextField tf_numLicence;
+	
+	@FXML
+	public void ajouterJoueur(Event e) {
+		//recherche joueur
+		TestJooueur j = ModeleJoueur.rechercherJoueur(Integer.parseInt(tf_numLicence.getText()));
+		//test si le joueur retourné n'est pas nulll !
+		if(data.contains(j))
+			data.add(j);
+		tf_numLicence.setText("");
+	}
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		TestJooueur j1 = new TestJooueur(1, "jean", "jacques");
+		TestJooueur j2 = new TestJooueur(2, "boubi", "baaa");
+		ModeleJoueur.ajouterJoueur(j1);
+		ModeleJoueur.ajouterJoueur(j2);
+
+		
+		listePersonne.setItems(data);
+		
+	}
+
+}
