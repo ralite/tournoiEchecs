1c79fd95c5948f679002430addb2dc67bf5c5a04
Nico <Nico@PC-Nico>
2015-11-26 10:58:32 +0100

    Ajout de fonctionnalités dans la fenetre controleurRecapTournoi

--- tournoiEchecs/src/controleur/ControleurRecapInfosTournoi.java
+++ tournoiEchecs/src/controleur/ControleurRecapInfosTournoi.java
@@ -3,9 +3,11 @@ package controleur;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import metier.Joueur;
 import metier.Tournoi;
 import modele.ModeleJoueur;
@@ -31,6 +33,13 @@ public class ControleurRecapInfosTournoi implements Initializable {
 	@FXML
 	Label label_recapNbRondes;
 
+	/*@FXML
+	ListView lv_recapJoueursInscrits;
+	
+	@FXML
+	ListView lv_recapDepartagesChoisis;
+	*/
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		label_recapNom.setText(ModeleTournoi.getTournoi().getNom());
@@ -38,7 +47,7 @@ public class ControleurRecapInfosTournoi implements Initializable {
 		label_recapArbitre.setText(ModeleTournoi.getTournoi().getArbitre());
 		label_recapDateDeb.setText(String.valueOf(ModeleTournoi.getTournoi().getDateDeb()));
 		label_recapDateFin.setText(String.valueOf(ModeleTournoi.getTournoi().getDateFin()));
-		label_recapNbRondes.setText(String.valueOf(ModeleTournoi.getTournoi().getNbRondes()));		
+		label_recapNbRondes.setText(String.valueOf(ModeleTournoi.getTournoi().getNbRondes()));
 	}
 
 }
