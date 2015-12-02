1d920e3d755609e11c987d294b479b7abc4dcb04
Nico <Nico@192.168.1.58>
2015-12-02 20:48:54 +0100

    ajout d'une imageView dans la fenetreAccueil (fond d'écran)

--- tournoiEchecs/src/controleur/ControleurFenetreAccueil.java
+++ tournoiEchecs/src/controleur/ControleurFenetreAccueil.java
@@ -6,16 +6,21 @@ import vue.FenetreAccueil;
 import vue.FenetreFileChooser;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import modele.ModeleTournoi;
 import application.Main;
 
-public class ControleurFenetreAccueil {
+public class ControleurFenetreAccueil implements Initializable {
 	
 	@FXML
 	private Button button_creerTournoi;
@@ -74,6 +79,16 @@ public class ControleurFenetreAccueil {
     	button_parcourirTournoi.setVisible(false);
     	button_creerTournoi.setVisible(false);
     	button_modifierJoueurAccueil.setVisible(true);
+    }
+    
+    @FXML
+    private ImageView imageView;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        File file = new File("src/echecs.jpg");
+        Image image = new Image(file.toURI().toString());
+        imageView.setImage(image);
     }
 
 }
