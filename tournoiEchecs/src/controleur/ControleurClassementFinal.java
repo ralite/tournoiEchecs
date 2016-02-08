f8e7e729f66662ed4ebb928636c764c09ac5c094
jérôme <jrme@jerome>
2016-02-08 16:11:11 +0100

    classement final

--- tournoiEchecs/src/controleur/ControleurClassementFinal.java
+++ tournoiEchecs/src/controleur/ControleurClassementFinal.java
@@ -12,6 +12,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 
 public class ControleurClassementFinal implements Initializable {
@@ -19,10 +20,21 @@ public class ControleurClassementFinal implements Initializable {
 	@FXML
 	ListView<Joueur> lv_classement;
 	
+	@FXML
+	Label lb_titre;
+	
+	@FXML
+	Label lb_classement;
+	
 	ObservableList<Joueur> itemsJoueur;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		int numRonde=ModeleTournoi.getTournoi().getNumRondeActuelle();
+		if(numRonde==-1)
+			numRonde=ModeleTournoi.getTournoi().getNbRondes();
+		lb_titre.setText(ModeleTournoi.getTournoi().getNom());
+		lb_classement.setText("Classement après la ronde "+numRonde);
 		itemsJoueur=FXCollections.observableArrayList();
 		itemsJoueur.addAll(ModeleTournoi.getTournoi().getListeJoueurs());
 		lv_classement.setItems(itemsJoueur);
