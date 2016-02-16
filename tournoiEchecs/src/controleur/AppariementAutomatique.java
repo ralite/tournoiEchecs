13d7ef93ac6a812d680f51a31a3b77fa2a0bd721
jérôme <jrme@jerome>
2016-02-16 20:07:39 +0100

    appariement automatique

--- tournoiEchecs/src/controleur/AppariementAutomatique.java
+++ tournoiEchecs/src/controleur/AppariementAutomatique.java
@@ -1,5 +1,7 @@
 package controleur;
 
+import java.rmi.Remote;
+
 import org.omg.CORBA.PRIVATE_MEMBER;
 
 import metier.Joueur;
@@ -16,40 +18,49 @@ public class AppariementAutomatique {
 	}
 
 	private static void calculAppariement(ObservableList<Joueur> joueurs,ObservableList<Partie> parties) {
-		int i =0;
-		int j = 1;
-		while(j<joueurs.size()){
-			if(i!=j-1){
-				parties.add(new Partie(joueurs.get(i), joueurs.get(j)));
-				i=j+1;
-				j+=2;
+		int i=0;
+		int numrondePrec = ModeleTournoi.getTournoi().getNumRondeActuelle()-1;
+		while(joueurs.size()>1){
+			int k = i+1;
+			while (ModeleTournoi.getTournoi().dejaRencontre(joueurs.get(i), joueurs.get(k)) && k<joueurs.size()){
+				k++;
 			}
-			else{
-				if(ModeleTournoi.getTournoi().dejaRencontre(joueurs.get(i), joueurs.get(j))){
-					j++;
-					parties.add(new Partie(joueurs.get(i), joueurs.get(j)));
-					i++;
-					j++;
-					
-				}
-				else{
-					parties.add(new Partie(joueurs.get(i), joueurs.get(j)));
-					i+=2;
-					j+=2;
-				}
-			}
+			if(joueurs.get(i).getCouleurRonde(numrondePrec).equals("B")&&joueurs.get(i).getCouleurRonde(numrondePrec-1).equals("B"))
+				parties.add(new Partie(joueurs.get(k), joueurs.get(i)));
+			else if(joueurs.get(i).getCouleurRonde(numrondePrec).equals("N")&&joueurs.get(i).getCouleurRonde(numrondePrec-1).equals("N"))
+					parties.add(new Partie(joueurs.get(i), joueurs.get(k)));
+				else if(joueurs.get(k).getCouleurRonde(numrondePrec).equals("B")&&joueurs.get(k).getCouleurRonde(numrondePrec-1).equals("B"))
+						parties.add(new Partie(joueurs.get(i), joueurs.get(k)));
+					else if(joueurs.get(k).getCouleurRonde(numrondePrec).equals("N")&&joueurs.get(k).getCouleurRonde(numrondePrec-1).equals("N"))
+							parties.add(new Partie(joueurs.get(k), joueurs.get(i)));
+						 else
+							 parties.add(new Partie(joueurs.get(i), joueurs.get(k)));
+			joueurs.removeAll(joueurs.get(i),joueurs.get(k));
+			
 			
 		}
-		setJoueurExempt(joueurs);
+		setJoueurExempt(joueurs,parties);
 		
 	}
 
-	private static void setJoueurExempt(ObservableList<Joueur> joueurs) {
+	private static void setJoueurExempt(ObservableList<Joueur> joueurs, ObservableList<Partie> parties) {
 		if(joueurs.size()%2!=0){
 			Joueur joueurExempt = joueurs.get(joueurs.size()-1);
+			if(joueurExempt.getCouleur().contains("X")){
+				Partie p = parties.get(parties.size()-1);
+				parties.remove(p);
+				if(p.getJoueurBlanc().getScore()<p.getJoueurNoir().getScore()){
+					parties.add(new Partie(joueurExempt, p.getJoueurNoir()));					
+				}
+				else{
+					parties.add(new Partie(p.getJoueurBlanc(), joueurExempt));
+				}
+			}
 			joueurs.clear();
 			joueurs.add(joueurExempt);
 		}
+		else
+			joueurs.clear();
 	}
 
 	private static void calculAppariementPremiereRonde(ObservableList<Joueur> joueurs, ObservableList<Partie> parties) {
@@ -62,7 +73,7 @@ public class AppariementAutomatique {
 			j++;
 			i++;
 		}
-		setJoueurExempt(joueurs);
+		setJoueurExempt(joueurs,parties);
 		
 	}
 	
