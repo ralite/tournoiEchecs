07b873eb60a27ec664dc5155506b3d177837d163
jérôme <jrme@jerome>
2016-02-18 10:06:38 +0100

    app auto

--- tournoiEchecs/src/controleur/AppariementAutomatique.java
+++ tournoiEchecs/src/controleur/AppariementAutomatique.java
@@ -18,25 +18,28 @@ public class AppariementAutomatique {
 	private static void calculAppariement(ObservableList<Joueur> joueurs,ObservableList<Partie> parties) {
 		int i=0;
 		boolean appImpossible=false;
-		while(joueurs.size()>1){
+		while(joueurs.size()>1 && !appImpossible){
 			int k = i+1;
-			while (k<joueurs.size()&& ModeleTournoi.getTournoi().dejaRencontre(joueurs.get(i), joueurs.get(k))||!appImpossible){
+			while (k<joueurs.size()&& ModeleTournoi.getTournoi().dejaRencontre(joueurs.get(i), joueurs.get(k))&&!appImpossible){
 				k++;
 				if(k==joueurs.size()){
 					k+=-1;
-					appImpossible=true;
-					Alert alert = new Alert(AlertType.WARNING);
-					alert.setTitle("Erreur");
-					alert.setContentText("Vueillez appairer les joueurs à la main s'il vous plait !");
-					alert.showAndWait();
+					appImpossible = appariementImpossible();
 
 				}
 			}
 			if(!appImpossible){
 				if(nbFoisJoueurJoueBlanc(joueurs.get(i))<nbFoisJoueurJoueBlanc(joueurs.get(k)))
 					parties.add(new Partie(joueurs.get(i),joueurs.get(k)));
+				else if(nbFoisJoueurJoueBlanc(joueurs.get(i))==nbFoisJoueurJoueBlanc(joueurs.get(k))){
+					if(joueurs.get(i).getCouleurRonde(ModeleTournoi.getTournoi().getNumRondeActuelle()-1).equals("B"))
+						parties.add(new Partie(joueurs.get(k),joueurs.get(i)));
+					else
+						parties.add(new Partie(joueurs.get(i),joueurs.get(k)));
+				}
 				else
 					parties.add(new Partie(joueurs.get(k),joueurs.get(i)));
+
 				joueurs.removeAll(joueurs.get(i),joueurs.get(k));
 			}
 
@@ -47,23 +50,56 @@ public class AppariementAutomatique {
 	}
 
 	private static void setJoueurExempt(ObservableList<Joueur> joueurs, ObservableList<Partie> parties) {
+		boolean appImpossible =false;
 		if(joueurs.size()%2!=0){
 			Joueur joueurExempt = joueurs.get(joueurs.size()-1);
 			if(joueurExempt.getCouleur().contains("X")){
 				Partie p = parties.get(parties.size()-1);
 				parties.remove(p);
-				if(p.getJoueurBlanc().getScore()<p.getJoueurNoir().getScore()){
-					parties.add(new Partie(joueurExempt, p.getJoueurNoir()));
+				if(!ModeleTournoi.getTournoi().dejaRencontre(joueurExempt, p.getJoueurBlanc())&&!ModeleTournoi.getTournoi().dejaRencontre(joueurExempt, p.getJoueurNoir())){
+					if(p.getJoueurBlanc().getScore()<p.getJoueurNoir().getScore()){
+						parties.add(new Partie(joueurExempt, p.getJoueurNoir()));
+						joueurExempt=p.getJoueurBlanc();
+					}
+					else{
+						parties.add(new Partie(p.getJoueurBlanc(), joueurExempt));
+						joueurExempt=p.getJoueurNoir();
+					}
 				}
 				else{
-					parties.add(new Partie(p.getJoueurBlanc(), joueurExempt));
+					if(ModeleTournoi.getTournoi().dejaRencontre(joueurExempt, p.getJoueurBlanc())){
+						parties.add(new Partie(joueurExempt, p.getJoueurNoir()));
+						joueurExempt=p.getJoueurBlanc();
+					}
+					else if(ModeleTournoi.getTournoi().dejaRencontre(joueurExempt, p.getJoueurNoir())){
+						parties.add(new Partie(joueurExempt, p.getJoueurBlanc()));
+						joueurExempt=p.getJoueurNoir();
+					}
+					else{
+						appImpossible = appariementImpossible();
+					}
 				}
 			}
-			joueurs.clear();
-			joueurs.add(joueurExempt);
+			if(!appImpossible){
+				joueurs.clear();
+				joueurs.add(joueurExempt);
+			}
+			else{
+				parties.clear();
+			}
 		}
 		else
 			joueurs.clear();
+	}
+
+	private static boolean appariementImpossible() {
+		boolean appImpossible;
+		appImpossible=true;
+		Alert alert = new Alert(AlertType.WARNING);
+		alert.setTitle("Erreur");
+		alert.setContentText("Vueillez appairer les joueurs à la main s'il vous plait !");
+		alert.showAndWait();
+		return appImpossible;
 	}
 
 	private static void calculAppariementPremiereRonde(ObservableList<Joueur> joueurs, ObservableList<Partie> parties) {
